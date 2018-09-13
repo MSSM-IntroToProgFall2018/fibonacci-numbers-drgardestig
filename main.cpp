f96216c38446ae105be8c1b7a7f8d8db549a53d7
Anders Gardestig <gardestiga@ADDC3.local>
2018-09-13 06:42:37 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Fibonacci rocks!" << endl;
+    int x = 1, y=1, z = 0;
+    while (y <= 1000000) {
+        cout << x << endl;
+        int fibonacci(int, int);
+        z = fibonacci(x,y);
+        x = y;
+        y = z;
+
+    }
     return 0;
+}
+
+int fibonacci(int a, int b){
+    return a+b;
 }
